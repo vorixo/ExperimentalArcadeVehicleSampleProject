f2945d0316f41624d2c1ea6b98e9ea0e7d63d2e3
Alvaro Jover <alvareke00@hotmail.com>
2021-04-19 11:25:50 +0200

    [UPD] Upgrading plugin version + removing un-needed submodules

--- Source/MyProject/MyProject.Build.cs
+++ Source/MyProject/MyProject.Build.cs
@@ -8,7 +8,7 @@ public class MyProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysXVehicles", "HeadMountedDisplay", "PhysicsCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysicsCore" });
 
 		PublicDefinitions.Add("HMD_MODULE_INCLUDED=1");
 	}
